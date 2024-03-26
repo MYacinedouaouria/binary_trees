9acaddf1b1d6f62c77e021c29355a0cf9d4b6631
MYacinedouaouria <yacine23i@hotmail.com>
2024-03-26 16:30:42 -0700

    solved check fail: if the parent node has a left then we should aslo initialize the parent of that left to the new node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,6 +19,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		new = binary_tree_node(parent, value);
 		if (new == NULL)
 			return (NULL);
+		parent->left->parent = new;
 		new->left = parent->left;
 		parent->left = new;
 	}
