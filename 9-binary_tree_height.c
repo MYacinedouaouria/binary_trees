7d25b96943c92842d2a856f789c001a36de79058
MYacinedouaouria <yacine23i@hotmail.com>
2024-03-27 16:58:06 -0700

    adde a check for the case where children of the root are NULL then the height is 0 not 1

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,14 +8,11 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int l, r;
-
-	if (tree == NULL)
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
-	l = binary_tree_height(tree->left);
-	r = binary_tree_height(tree->right);
-	if (l >= r)
-		return (l + 1);
-	else
-		return (r + 1);
+	size_t l = binary_tree_height(tree->left);
+
+	size_t r = binary_tree_height(tree->right);
+
+	return ((l > r ? l : r) + 1);
 }
